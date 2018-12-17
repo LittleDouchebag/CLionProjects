67a4a3053bcd47af72e8903bc7218399b0ea8384
LittleDouchebag <LittleDouchebag@users.noreply.github.com>
2018-12-17 21:55:44 +0800

    C++ fight
    never say never my friend

--- Oops/main.cpp
+++ Oops/main.cpp
@@ -1,6 +1,25 @@
+#include <limits>
 #include <iostream>
+#include <cstring>
+using namespace std;
 
+inline void fun(int);
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    struct happy{
+        int i=0;
+        int count = 100;
+    };
+    happy *happy_me = new happy;
+    happy_me->i++;
+    happy *p = new happy;
+    fun(100);
+    if(p && p->count > 1)
+        cout<<p<<endl;
+}
+void shutip();
+inline void fun(int happy){
+    return shutip();
+}
+void shutip(){
+    return;
 }
