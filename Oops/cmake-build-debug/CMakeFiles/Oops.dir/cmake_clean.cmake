78047ab48d1520a0889de1b626ce2b3f36254fd8
LittleDouchebag <LittleDouchebag@users.noreply.github.com>
2018-12-17 11:16:38 +0800

    Oops

---
+++ Oops/cmake-build-debug/CMakeFiles/Oops.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oops.dir/main.cpp.o"
+  "Oops.pdb"
+  "Oops.exe"
+  "libOops.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oops.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
