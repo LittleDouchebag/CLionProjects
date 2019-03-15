f0a597b269ad85db3c3da45c890e0ecf5bda9eb9
LittleDouchebag <LittleDouchebag@users.noreply.github.com>
2019-03-15 09:48:34 +0800

    123

--- Oops/main.cpp
+++ Oops/main.cpp
@@ -3,23 +3,9 @@
 #include <cstring>
 using namespace std;
 
-inline void fun(int);
 int main() {
-    struct happy{
-        int i=0;
-        int count = 100;
-    };
-    happy *happy_me = new happy;
-    happy_me->i++;
-    happy *p = new happy;
-    fun(100);
-    if(p && p->count > 1)
-        cout<<p<<endl;
-}
-void shutip();
-inline void fun(int happy){
-    return shutip();
-}
-void shutip(){
-    return;
+    int (*p1)[5];
+    int *p2[5];
+
+    1;
 }
